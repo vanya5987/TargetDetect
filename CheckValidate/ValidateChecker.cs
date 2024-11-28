d26a2c47f539cca647270b7f32d96175756bc281
vanya5987 <vanya5987@mail.ru>
2024-11-29 02:36:36 +0300

    Общая модель программы

---
+++ CheckValidate/ValidateChecker.cs
@@ -0,0 +1,65 @@
+﻿using DirectShowLib;
+
+namespace TestTask.CheckValidate
+{
+    internal class ValidateChecker
+    {
+        private readonly List<Point> _landmarks;
+        private int _maxLandmarks;
+        private int _minWidth;
+        private int _minHeight;
+
+        public ValidateChecker(List<Point> landmarks, int maxLandmarks, int minWidth, int minHeight)
+        {
+            _landmarks = landmarks ?? throw new ArgumentNullException(nameof(_landmarks));
+            _maxLandmarks = maxLandmarks;
+            _minWidth = minWidth;
+            _minHeight = minHeight;
+
+            if (_maxLandmarks < 0)
+                throw new IndexOutOfRangeException(nameof(_maxLandmarks));
+            if (_minHeight < 0)
+                throw new IndexOutOfRangeException(nameof(_minHeight));
+            if (_minWidth < 0)
+                throw new IndexOutOfRangeException(nameof(_minWidth));
+        }
+
+        public void CheckValidateRectangle()
+        {
+            if (_landmarks.Count < _maxLandmarks)
+                return;
+
+            int left = _landmarks.Min(p => p.X);
+            int right = _landmarks.Max(p => p.X);
+            int top = _landmarks.Min(p => p.Y);
+            int bottom = _landmarks.Max(p => p.Y);
+
+            int width = right - left;
+            int height = bottom - top;
+
+            if (width < _minWidth)
+                right = left + _minWidth;
+            if (height < _minHeight)
+                bottom = top + _minHeight;
+            if (_landmarks.Count < _maxLandmarks)
+                throw new InvalidOperationException("Недостаточно точек для формирования цели.");
+
+            _landmarks[0] = new Point(left, top);
+            _landmarks[1] = new Point(right, top);
+            _landmarks[2] = new Point(left, bottom);
+            _landmarks[3] = new Point(right, bottom);
+        }
+
+        public void CheckValidateCams(ToolStripComboBox cameraChoice,DsDevice[] cams)
+        {
+            if (cams == null || cams.Length == 0)
+                throw new IndexOutOfRangeException("Камеры не найдены...");
+
+            if (cameraChoice.SelectedItem == null)
+            {
+                MessageBox.Show("Пожалуйста, выберите камеру.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
---
+++ PictureFilters/SobelFilter.cs
@@ -0,0 +1,37 @@
+﻿namespace TestTask.PictureFilters
+{
+    internal class SobelFilter
+    {
+        public  Bitmap ApplySobelEdgeDetection(Bitmap input)
+        {
+            Bitmap edgeImage = new Bitmap(input.Width, input.Height);
+
+            int[,] sobelX = new int[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] sobelY = new int[,] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
+
+            for (int x = 1; x < input.Width - 1; x++)
+            {
+                for (int y = 1; y < input.Height - 1; y++)
+                {
+                    int gx = 0, gy = 0;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int pixelValue = input.GetPixel(x + i, y + j).R;
+                            gx += pixelValue * sobelX[i + 1, j + 1];
+                            gy += pixelValue * sobelY[i + 1, j + 1];
+                        }
+                    }
+
+                    int magnitude = (int)Math.Sqrt(gx * gx + gy * gy);
+                    magnitude = Math.Min(255, Math.Max(0, magnitude));
+                    edgeImage.SetPixel(x, y, Color.FromArgb(magnitude, magnitude, magnitude));
+                }
+            }
+
+            return edgeImage;
+        }
+    }
+}
